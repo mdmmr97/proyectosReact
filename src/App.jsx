ec6cedb00437c80fdcfb5ca73edf7050afc3ca2d
mdmmr97 <mdmmr97@hotmail.com>
2023-12-11 10:28:56 +0100

    Añadida Imagen

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Imagen from './componentes/Imagen'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const tweet = 'Comenzamos con React en el módulo de Desarrollo web en entorno cliente. 2ª Evaluación';
+  const nombre = 'Víctor M. Garrido';
+  const nick   ='@vgc02g';
+  const cuando = '3h ago ';
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="tweet">
+      <Imagen></Imagen>
+        <div className="content">
+          <span className="author" >
+            <span className="name" > {nombre} </span>
+            <span className="handle" > {nick} </span>
+            <span className="time" > {cuando} </span>
+          </span>
+          <div className="message" >
+            {tweet}
+          </div>    
+          <div className="buttons" >
+          <i className="fa fa-reply reply-button" />
+          <i className="fa fa-retweet retweet-button" />
+          <i className="fa fa-heart like-button" />
+          <i className="fa fa-ellipsis-h more-options-button" />
+          </div>            
+        </div>                
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-const App = () => {
-  const tweet = 'Comenzamos con React en el módulo de Desarrollo web en entorno cliente. 2ª Evaluación';
-  const nombre = 'Víctor M. Garrido';
-  const nick   ='@vgc02g';
-  const cuando = '3h ago ';
-
-  return (
-    <div className="tweet">
-      <img
-        src="https://www.gravatar.com/avatar/763dcd4dad38689d57aa9d83a60bfaa5.jpg"
-        className="avatar"
-        alt="avatar" />
-        <div className="content">
-          <span className="author" >
-            <span className="name" > {nombre} </span>
-            <span className="handle" > {nick} </span>
-            <span className="time" > {cuando} </span>
-          </span>
-          <div className="message" >
-            {tweet}
-          </div>    
-          <div className="buttons" >
-          <i className="fa fa-reply reply-button" />
-          <i className="fa fa-retweet retweet-button" />
-          <i className="fa fa-heart like-button" />
-          <i className="fa fa-ellipsis-h more-options-button" />
-          </div>            
-        </div>                
-    </div>
-  )
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
