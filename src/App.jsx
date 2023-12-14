1c82d7a295dfa10bf0f0c80e2def0f4cada8a60e
mdmmr97 <mdmmr97@hotmail.com>
2023-12-14 13:33:25 +0100

    Ejemplo Objeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Contador from './componentes/Contador';
+import Contador2 from './componentes/Contador2';
 
 function App() {
 
   return (
-    <Contador contador={5}></Contador>
+    //<Contador contador={5}></Contador>
+    <Contador2></Contador2>
   )
 }
 
