91d16b631491fc8d3f5416d7965c841c3300afc5
mdmmr97 <mdmmr97@hotmail.com>
2023-12-14 13:12:46 +0100

    Ejemlos terminados

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -3,23 +3,29 @@ import React, { useState } from "react";
 const Contador = (props) => {
 
     const [contador, setContador] = useState(+props.contador);
+    const [clicks, setClicks] = useState([]);
 
     function incrementarContador() {
         setContador(contador+1)
+        setClicks(clicks.concat("i"));
     }
 
     function resetearContador() {
         setContador(+props.contador)
+        setClicks([...clicks, "r"]);
     }
 
-    function mostrarMensaje() {
-        return contador % 2 === 0 ? "Es Par" : "Es Impar"
-    }
+        const esPar = contador % 2 === 0;
+        const mensaje = esPar ? "Es Par" : "Es Impar";
 
     return (
         <div>
             <h1>{contador}</h1>
-            <p>{mostrarMensaje(contador)}</p>
+            <p>{mensaje}</p>
+            <p>{clicks}</p>
+            <p> Nº Clicks: {clicks.length}</p>
+            <p> Clicks "I": {clicks.filter(e => e === "i").length}</p>
+            <p> Clicks "R": {clicks.filter(e => e === "r").length}</p>
             <button onClick={incrementarContador}> Incrementar Contador</button>
             <button onClick={resetearContador}> Resetear Contador</button>
         </div>
