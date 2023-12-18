5859abb2235ed1ff8fdd8aad2556aee4ec726d9b
mdmmr97 <mdmmr97@hotmail.com>
2023-12-18 11:22:18 +0100

    Ejemplo de listar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Coche from './Componentes/Coches';
+import losCoches from './mock-coches';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function ordenar(coche1, coche2){
+
+   // return coche2.npuertas - coche1.npuertas;
+
+    return (coche1.modelo < coche2.modelo) ? -1 : 1;
+
+  }
+
+  function muestraCoches(coche) {  
+
+    return <Coche  key={coche.matricula} coche={coche}></Coche>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Modelo</th>
+          <th>Matricula</th>
+          <th>Color</th>
+          <th>Puertas</th>
+        </tr>
+      </thead>
+      <tbody >
+      {losCoches.toSorted(ordenar).map(muestraCoches)}
+      </tbody>
+    </table>
+  );
 }
-
-export default App
+export default App;
