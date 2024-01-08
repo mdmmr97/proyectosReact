15fea6c853be60c87aba4f945aa0a3bf3d0d0581
mdmmr97 <mdmmr97@hotmail.com>
2024-01-08 11:39:03 +0100

    Ejemplo formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Coche from './componentes/Coche';
+import CocheForm from './componentes/Cocheform';
+import losCoches from './mock-coches';
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [listaCoches, setListaCoches] = useState(losCoches);
+
+  function muestraCoches(coche) {  
+
+    return <Coche  key={coche.matricula} coche={coche}></Coche>;
+    
+  }
+
+  // Esta función se envía al componente hijo como prop
+  function manejarAccion(coche) {
+
+    console.log("valor componetne hijo ", coche, " lo añadimos a la lista");
+
+    // Añadimos el coche a la lista de coches
+    setListaCoches([...listaCoches, coche]);
+
 }
-
-export default App
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Modelo</th>
+            <th>Matricula</th>
+            <th>Color</th>
+            <th>Puertas</th>
+          </tr>
+        </thead>
+        <tbody >
+        {listaCoches.map(muestraCoches)}
+        </tbody>
+      </table>
+      {/*Enviamos al componente hijo la función mamnejarAccion como prop*/}
+      <CocheForm manejarAccion={manejarAccion}></CocheForm>
+    </div>
+    );
+}
+export default App;
