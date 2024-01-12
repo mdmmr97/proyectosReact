e4d21c497229d32a5051366ef5ebb329d073bbf8
mdmmr97 <mdmmr97@hotmail.com>
2024-01-12 13:05:16 +0100

    Ejercicio 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import BarraHeramientas from './componentes/BarraHerramientas';
+import TemaContext from './contextos/TemaContext';
+import IdiomaContext from './contextos/IdiomaContex';
+import temas from './mock-temas';
+import SelectorTema from './componentes/SelectorTema';
+import SelectorIdioma from './componentes/SelectorIdioma';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const ES="es";
+
+  const [temaElegido, setTemaElegido] = useState(temas.light);
+  const [idiomaElegido, setIdiomaElegido] = useState(ES);
+
+  function manejarSeleccionTema(valor)  {
+    setTemaElegido(valor==="1"?temas.light:temas.dark);
+  }
+
+  function manejarSeleccionIdioma(valor)  {
+    setIdiomaElegido(valor);
+  }  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <SelectorIdioma idioma={idiomaElegido}  manejarSeleccion={manejarSeleccionIdioma}></SelectorIdioma>
+      <IdiomaContext.Provider value={idiomaElegido}>
+        <SelectorTema tema={1} manejarSeleccion={manejarSeleccionTema} ></SelectorTema>
+        {/*Definimos el proveedor del contexto.*/}
+        <TemaContext.Provider value={temaElegido}>
+          <BarraHeramientas></BarraHeramientas>
+        </TemaContext.Provider>
+      </IdiomaContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/componentes/SelectorTema.jsx
@@ -0,0 +1,29 @@
+import React, {useContext} from "react";
+import {useState} from "react";
+import IdiomaContext from "../contextos/IdiomaContex";
+import idiomas from "../mock-idiomas";
+
+const SelectorTema = (props) => {             
+
+    const TEMA1 = "tema1";
+    const TEMA2 = "tema2";
+
+    // Usamos el contexto.
+    const idioma = useContext(IdiomaContext);  
+
+    const [temaSeleccionado, setTemaSeleccionado] = useState(props.tema);
+
+    function asignarTema(event){
+        props.manejarSeleccion(event.target.value);        
+        setTemaSeleccionado(event.target.value);
+
+    }
+
+    return (
+        <div  onChange={asignarTema} >
+            <input id={TEMA1} type="radio" value={1} name="tema" defaultChecked={temaSeleccionado===1}/> {idiomas[idioma].tema.op1}
+            <input id={TEMA2} type="radio" value={2} name="tema" defaultChecked={temaSeleccionado===2}/> {idiomas[idioma].tema.op2}
+        </div>
+    )
+}
+export default SelectorTema;
