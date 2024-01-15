583b2e78697f3eca4f6d57ee999d07ee8c8f9bbb
mdmmr97 <mdmmr97@hotmail.com>
2024-01-15 10:45:51 +0100

    añadir Datos a Servidor

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Post from './componentes/Post'
+import PostForm from './componentes/PostForm';
+import { getAllPosts } from './servicios/posts/getAllPosts';
+import { newPost } from './servicios/posts/newPosts';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // Estado con la lista de post que recuperamos de la REST API
+  const [listaPost, setListaPost] = useState([]);
+
+  function obtenerPosts(){
+
+      // Usamos el servicio de obtención de posts que hemos creado
+      getAllPosts().then(posts => {
+
+          //Cargamos los post en el estado del componente
+          setListaPost(posts);
+        });                    
+  }
+
+  // Llamamos a la función de extracción de datos con un useEffect
+  // para que solo se ejecute una vez
+  useEffect(obtenerPosts, []);
+
+  // Esta función se envía al componente hijo como prop para la 
+  // inserción de una nueva fruta
+  function manejarAccion(post) {
+
+    console.log("valor componente hijo ", post, " lo añadimos a la lista");
+
+    newPost(post).then(post => {
+
+      console.log(post);
+      
+      // Añadimos el post a la lista
+      setListaPost([...listaPost, post]);
+
+    });
+    
+  }  
+
+  // Función encargada de llamar al componente Post con el post
+  // que recibe como parámetro implícito de la función map
+  function muestraPost(post) {  
+
+    return <Post  key={post.id} post={post}></Post>;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div id="divPost">
+        <h1>Post</h1>
+        <ul>                        
+        {listaPost.map(muestraPost)}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="divForm">
+        <PostForm manejarAccion={manejarAccion}>
+        </PostForm>        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+
+    </div>
+    );
+}
+export default App;
