9fd5826119fb5ff0a4167820e7640bf65a2af61a
mdmmr97 <mdmmr97@hotmail.com>
2024-01-19 12:54:17 +0100

    enunciado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css';
-import TodosPostAZ from './componentes/TodosPostAZ.jsx';
-import TodosPostZA from './componentes/TodosPostZA.jsx';
-
+import MouseApp from './componentes/MouseApp';
 function App() {
+
 
   return (
     <div>
-      <div id="divPost">
-        <TodosPostAZ></TodosPostAZ>
-      </div>
-      <div id="divPost">
-        <TodosPostZA></TodosPostZA>
-      </div>
+      <MouseApp></MouseApp>
     </div>
     );
 }
