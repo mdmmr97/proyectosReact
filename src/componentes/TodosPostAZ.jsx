b0f8e77b7dc86edbda8840b38ed796d18d3025a0
mdmmr97 <mdmmr97@hotmail.com>
2024-01-19 12:49:28 +0100

    hooks personalizados

--- src/componentes/TodosPostAZ.jsx
+++ src/componentes/TodosPostAZ.jsx
@@ -1,39 +1,13 @@
-import { useEffect, useState } from 'react';
-import { getAllPosts } from '../servicios/posts/getAllPosts';
 
 import AjaxLoader from './AjaxLoader.jsx';
+
+import usePost from '../hooks/usePost.jsx';
 
 import Post from './Post.jsx'
 
 const TodosPostAZ= () => {
 
-  // Estado con la lista de post que recuperamos de la REST API
-  const [listaPost, setListaPost] = useState([]);
-
-  // Estado para controlar si estamos cargando los datos o hemos finalizado
-  // de cargarlos
-  const [buscando, setBuscando] = useState(false);
-
-  function obtenerPosts(){
-
-    //Marcamos que estamos buscando los datos
-    setBuscando(true);
-
-    // Usamos el servicio de obtención de posts que hemos creado
-    getAllPosts().then(posts => {
-
-        //Cargamos los post en el estado del componente
-        setListaPost(posts);
-
-        //Indicamos que hemos terminado de cargar los datos
-        setBuscando(false);
-
-      });                    
-    }
-
-  // Llamamos a la función de extracción de datos con un useEffect
-  // para que solo se ejecute una vez
-  useEffect(obtenerPosts, []);
+  const posts = usePost();
 
   // Función encargada de llamar al componente Post con el post
   // que recibe como parámetro implícito de la función map
@@ -42,19 +16,13 @@ const TodosPostAZ= () => {
     return <Post  key={post.id} post={post}></Post>;
 
   }
-
-    return (
-
+   return (
         <div>
             <h1>Todos Post AZ</h1>
-            {buscando ? <AjaxLoader></AjaxLoader> 
-			          : <ul>{listaPost.map(muestraPost)}</ul>}
+            {posts.buscando ? <AjaxLoader></AjaxLoader> 
+                            : <ul>{posts.listaPost.map(muestraPost)}</ul>}
       </div>
-
-
-
     )
-
 }
 
 export default TodosPostAZ;
--- src/componentes/TodosPostZA.jsx
+++ src/componentes/TodosPostZA.jsx
@@ -1,46 +1,16 @@
-import { useEffect, useState } from 'react';
-import { getAllPosts } from '../servicios/posts/getAllPosts';
-
 import AjaxLoader from './AjaxLoader.jsx';
-
+import usePost from '../hooks/usePost.jsx';
 import Post from './Post.jsx'
 
-const TodosPostZA= () => {
+const TodosPostAZ= () => {
 
-  // Estado con la lista de post que recuperamos de la REST API
-  const [listaPost, setListaPost] = useState([]);
-
-  // Estado para controlar si estamos cargando los datos o hemos finalizado
-  // de cargarlos
-  const [buscando, setBuscando] = useState(false);
-
-  function obtenerPosts(){
-
-    //Marcamos que estamos buscando los datos
-    setBuscando(true);
-
-    // Usamos el servicio de obtención de posts que hemos creado
-    getAllPosts().then(posts => {
-
-        //Cargamos los post en el estado del componente
-        setListaPost(posts);
-
-        //Indicamos que hemos terminado de cargar los datos
-        setBuscando(false);
-
-      });                    
-    }
-
-  // Llamamos a la función de extracción de datos con un useEffect
-  // para que solo se ejecute una vez
-  useEffect(obtenerPosts, []);
+  const posts = usePost();
 
   // Función encargada de llamar al componente Post con el post
   // que recibe como parámetro implícito de la función map
   function muestraPost(post) {  
 
     return <Post  key={post.id} post={post}></Post>;
-
   }
 
   function sortZA(p1, p2) {
@@ -55,8 +25,8 @@ const TodosPostZA= () => {
 
         <div>
             <h1>Todos Post AZ</h1>
-            {buscando ? <AjaxLoader></AjaxLoader> 
-			          : <ul>{listaPost.toSorted(sortZA).map(muestraPost)}</ul>}
+            {posts.buscando ? <AjaxLoader></AjaxLoader> 
+                            : <ul>{posts.listaPost.toSorted(sortZA).map(muestraPost)}</ul>}
       </div>
 
 
@@ -65,4 +35,4 @@ const TodosPostZA= () => {
 
 }
 
-export default TodosPostZA;
+export default TodosPostAZ;
