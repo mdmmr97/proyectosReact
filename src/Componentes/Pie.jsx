e6fcbfcc19513495b2ed136efc38f47c9de9193e
mdmmr97 <mdmmr97@hotmail.com>
2024-01-29 13:52:01 +0100

    Maquetado pie

--- src/Componentes/Pie.jsx
+++ src/Componentes/Pie.jsx
@@ -1,14 +1,45 @@
+import Logo from '../assets/Imagenes/mp-logoNaranja100.png'
+
 const Pie = () => {
     return (
         <footer>
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col">
-                        <hr />
-                        <p className="text-center">
-                            Todos los derechos reservados &copy; 2021
-                        </p>
-                    </div>
+                <div className="d-flex justify-content-center">
+                    <div><p>hola</p></div>
+                    <div><p>hola</p></div>
+                    <div><p>hola</p></div>
+                </div>
+                <div className="text-center">
+                    <a href="https://cifpcarlos3.es/es">
+                        <h2>CIFP Carlos III</h2>
+                    </a>
+                    <p>
+                        C/ Carlos III, 30201 - Cartagena | <a href="mailto:30019702@murciaeduca.es">30019702@murciaeduca.es</a> | <a href="tel:968321301">968321301</a>
+                    </p>
+                </div>
+                <div className="text-center">  
+                    <a href="https://www.facebook.com/cifpcarlos3">
+                        <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="https://www.twitter.com/cifpcarlos3">
+                        <i className="bi bi-twitter-x"></i>
+                    </a>
+                    <a href="https://www.instagram.com/cifpcarlos3">
+                        <i className="bi bi-instagram"></i>
+                    </a>
+                    <a href="https://www.tiktok.com/@cifpcarlos3">
+                        <i className="bi bi-tiktok"></i>
+                    </a>
+                    <a href="https://www.youtube.com/c/cifpcarlosiiicartagena">
+                        <i className="bi bi-youtube"></i>
+                    </a> 
+                </div>
+                <hr/>
+                <div className="d-flex justify-content-center">
+                    <img src={Logo} alt="Logo Pagina" width={40} height={40}/>
+                    <p>
+                        Marca Personal FP | Diseño Web CFGS Desarrollo de Aplicaciones Web © 2023
+                    </p>
                 </div>
             </div>
         </footer>
